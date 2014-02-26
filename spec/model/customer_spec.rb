9016fbb03f9aa2379e66f9a3983b124d9c65de31
keiyan9 <nanbakouichi@nanba-no-MacBook-Air.local>
2014-02-26 22:59:53 +0900

    1.4

--- spec/model/customer_spec.rb
+++ spec/model/customer_spec.rb
@@ -16,23 +16,4 @@ describe Customer do
     subject{ customer.statement }
     it{ should == "Rental Record for #{name}\n\t#{movie_title}\t14.0\nAmount owed is 14.0\nYou earned 1 frequent renter points" }
   end
-
-  describe 'amount_for(rental)' do
-    let(:movie){ Movie.new('movie-title', price_code) }
-    let(:days_rented){ 10 }
-    let(:rental){ Rental.new(movie, days_rented) }
-    subject{ customer.amount_for(rental) }
-    context '一般の場合' do
-      let(:price_code){ 0 }
-      it{ should == 14.0 }
-    end
-    context '新作の場合' do
-      let(:price_code){ 1 }
-      it{ should == 30.0 }
-    end
-    context '子供用の場合' do
-      let(:price_code){ 2 }
-      it{ should == 12.0 }
-    end
-  end
 end
