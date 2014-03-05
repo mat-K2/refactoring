c4fbe22f3db4d21d6381df3831565d92983557ff
keiyan9 <nanbakouichi@nanba-no-MacBook-Air.local>
2014-03-05 23:54:22 +0900

    6.5-after(replace temp with chain)

--- app/models/select.rb
+++ app/models/select.rb
@@ -1,9 +1,16 @@
 class Select
+  def self.with_option(option)
+    select = self.new
+    select.options << option
+    select
+  end
+
   def options
     @options ||= []
   end
 
-  def add_option(arg)
+  def and(arg)
     options << arg
+    self
   end
 end
--- spec/model/select_spec.rb
+++ spec/model/select_spec.rb
@@ -3,12 +3,7 @@ require 'spec_helper'
 describe Select do
   let(:select){ Select.new }
   describe 'add_option(arg)' do
-    before do
-      select.add_option(1999)
-      select.add_option(2000)
-      select.add_option(2001)
-      select.add_option(2002)
-    end
-    it{ select.options.should == [1999, 2000, 2001, 2002] }
+    subject{ Select.with_option(1999).and(2000).and(2001).and(2002) }
+    it{ subject.options.should == [1999, 2000, 2001, 2002] }
   end
 end
