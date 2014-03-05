00b880c70c58df4d8ec9fcb5927f35b2e08e4a05
keiyan9 <nanbakouichi@nanba-no-MacBook-Air.local>
2014-03-05 22:59:22 +0900

    6.4-before(replace temp with query)

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,13 +1,28 @@
 class Order
   attr_reader :amount
   attr_reader :base_price
+  attr_accessor :quantity
+  attr_accessor :item_price
 
-  def initialize (amount, base_price)
+  def initialize (amount, base_price, quantity=nil, item_price=nil)
     @amount = amount
     @base_price = base_price
+    @quantity = quantity
+    @item_price = item_price
   end
 
   def over_thousand?
     self.base_price > 1000
+  end
+
+  def price
+    base_price = @quantity * @item_price
+
+    if base_price > 1000
+      discount_factor = 0.95
+    else
+      discount_factor = 0.98
+    end
+    base_price * discount_factor
   end
 end
--- spec/model/order_spec.rb
+++ spec/model/order_spec.rb
@@ -3,7 +3,9 @@ require 'spec_helper'
 describe Order do
   let(:amount){ 100 }
   let(:base_price){ 1000 }
-  let(:order){ Order.new(amount, base_price) }
+  let(:quantity){ nil }
+  let(:item_price){ nil }
+  let(:order){ Order.new(amount, base_price, quantity, item_price) }
   describe 'over_thousand?' do
     subject{ order.over_thousand? }
     context 'base_price > 1000' do
@@ -13,6 +15,20 @@ describe Order do
     context 'base_price <= 1000' do
       let(:base_price){ 1000 }
       it{ should be_false }
+    end
+  end
+
+  describe 'price' do
+    subject{ order.price }
+    context 'base_price > 1000' do
+      let(:quantity){ 1 }
+      let(:item_price){ 1001 }
+      it{ should == 1001 * 0.95 }
+    end
+    context 'base_price <= 1000' do
+      let(:quantity){ 1 }
+      let(:item_price){ 1000 }
+      it{ should == 1000 * 0.98 }
     end
   end
 end
