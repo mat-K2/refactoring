8a4eaa7eca18eb2c441c3511688f68058180fb95
keiyan9 <nanbakouichi@nanba-no-MacBook-Air.local>
2014-03-09 17:30:42 +0900

    6.7-after

--- app/models/haggis.rb
+++ app/models/haggis.rb
@@ -4,15 +4,38 @@ class Haggis
   end
 
   def distance_traveled(time)
-    primary_acc = @primary_force / @mass
-    primary_time = [time, @delay].min
-    result = 0.5 * primary_acc * primary_time * primary_time
-    secondary_time = time - @delay
-    if (secondary_time > 0)
-      primary_vel = primary_acc * @delay
-      secondary_acc = (@primary_force + @secondary_force) / @mass
-      result += primary_vel * secondary_time + 5 * secondary_acc * secondary_time * secondary_time
-    end
-    result
+    accelerate_secondary?(time) ? primary_distance_traveled(time) + secondary_distance_traveled(time) : primary_distance_traveled(time)
+  end
+
+  def accelerate_secondary?(time)
+    secondary_time(time) > 0
+  end
+
+  def primary_distance_traveled(time)
+    0.5 * primary_acc * primary_time(time) * primary_time(time)
+  end
+
+  def secondary_distance_traveled(time)
+    primary_vel * secondary_time(time) + 5 * secondary_acc * secondary_time(time) * secondary_time(time)
+  end
+
+  def primary_acc
+    @primary_force / @mass
+  end
+
+  def secondary_acc
+    (@primary_force + @secondary_force) / @mass
+  end
+
+  def primary_time(time)
+    [time, @delay].min
+  end
+
+  def secondary_time(time)
+    time - @delay
+  end
+
+  def primary_vel
+    primary_acc * @delay
   end
 end
--- spec/model/haggis_spec.rb
+++ spec/model/haggis_spec.rb
@@ -5,16 +5,56 @@ describe Haggis do
   let(:secondary_force){ 20 }
   let(:mass){ 2 }
   let(:delay){ 5 }
+  let(:time){ 10 }
   let(:haggis){ Haggis.new(primary_force, secondary_force, mass, delay) }
+
   describe 'distance_traveled(time)' do
+    let(:primary_distance_traveled){ 62.5 }
+    let(:secondary_distance_traveled){ 2000 }
+    before do
+      haggis.should_receive(:accelerate_secondary?).with(time).and_return(accelerate_secondary_key)
+      haggis.should_receive(:primary_distance_traveled).with(time).and_return(primary_distance_traveled)
+    end
     subject{ haggis.distance_traveled(time) }
-    context 'secondary_time <= 0' do
-      let(:time){ 5 }
+
+    context 'not accelerate secondary' do
+      let(:accelerate_secondary_key){ false }
+      before do
+        haggis.should_not_receive(:secondary_distance_traveled)
+      end
       it{ should == 62.5 }
     end
-    context 'secondary_time > 0' do
-      let(:time){ 6 }
-      it{ should == 162.5 }
+    context 'accelerate secondary' do
+      let(:accelerate_secondary_key){ true }
+      before do
+        haggis.should_receive(:secondary_distance_traveled).with(time).and_return(secondary_distance_traveled)
+      end
+      it{ should == 2062.5 }
     end
+  end
+
+  describe 'accelerate_secondary?(time)' do
+    before do
+      haggis.should_receive(:secondary_time).with(time).and_return(secondary_time)
+    end
+    subject{ haggis.accelerate_secondary?(time) }
+    context 'secondary_time > 0' do
+      let(:secondary_time){ 1 }
+      it{ should be_true }
+    end
+    context 'secondary_time <= 0' do
+      let(:secondary_time){ 0 }
+      it{ should be_false }
+    end
+  end
+
+  describe 'primary_distance_traveled(time)' do
+    subject{ haggis.primary_distance_traveled(time) }
+    it{ should == 62.5 }
+  end
+
+  describe 'secondary_distance_traveled(time)' do
+    subject{ haggis.secondary_distance_traveled(time) }
+    it{ should == 2000 }
   end
 end
