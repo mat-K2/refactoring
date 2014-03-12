9163ba071e5bc3b809aa58d1b0cdeeb624ce8c6d
keiyan9 <nanbakouichi@nanba-no-MacBook-Air.local>
2014-03-12 22:48:57 +0900

    refactor substitute algorithm

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,17 +1,5 @@
 class Person
   def found_friends(people)
-    friends = []
-    people.each do |person|
-      if person == 'Don'
-        friends << person
-      end
-      if person == 'John'
-        friends << person
-      end
-      if person == 'Kent'
-        friends << person
-      end
-    end
-    return friends
+    people.select { |person| %w(Don John Kent).include?(person) }
   end
 end
