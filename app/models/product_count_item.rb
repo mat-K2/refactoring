9f7f43dd401d6524a14267fa14f6fa25f619eac1
keiyan9 <nanbakouichi@nanba-no-MacBook-Air.local>
2014-03-17 06:14:00 +0900

    refactor remove unused default parameter

--- app/models/product_count_item.rb
+++ app/models/product_count_item.rb
@@ -1,6 +1,5 @@
 class ProductCountItem
-  def product_count_items(search_criteria=nil)
-    criteria = search_criteria || @search_criteria
-    ProductCountItem.find_all_by_criteria(criteria)
+  def product_count_items(search_criteria)
+    ProductCountItem.find_all_by_criteria(search_criteria)
   end
 end
--- spec/model/product_count_item_spec.rb
+++ spec/model/product_count_item_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe ProductCountItem do
   let(:search_criteria){ SearchCriteria.new({ :author_id => 1, :publisher_id => 2, :isbn => 123456 }) }
   let(:product_count_items){ ProductCountItem.new }
-  describe 'product_count_items(search_criteria=nil)' do
+  describe 'product_count_items(search_criteria)' do
     let(:find_result){ [{ 'id' => 1 }] }
     before do
       ProductCountItem.should_receive(:find_all_by_criteria).with(search_criteria).and_return(find_result)
