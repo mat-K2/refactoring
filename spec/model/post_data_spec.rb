da6be2c3d9af8ca821b085b5701152b958d3806d
keiyan9 <nanbakouichi@nanba-no-MacBook-Air.local>
2014-03-18 09:01:48 +0900

    6.17-before(2)

---
+++ spec/model/post_data_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe PostData do
+  let(:post_data_val){ {
+      :params => 'params-data',
+      :session => 'session-data',
+      :date => 'date-data'
+    }
+  }
+  let(:post_data){ PostData.new(post_data_val) }
+
+  describe 'params' do
+    subject{ post_data.params }
+    it{ should == 'params-data' }
+  end
+
+  describe 'session' do
+    subject{ post_data.session }
+    it{ should == 'session-data' }
+  end
+
+  describe 'date' do
+    subject{ post_data.date }
+    specify{ expect{ subject }.to raise_error(NoMethodError) }
+  end
+end
