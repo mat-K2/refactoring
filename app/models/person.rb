5e25bb6d6d710b0106dca7969a35822ae3b007d2
keiyan9 <nanbakouichi@nanba-no-MacBook-Air.local>
2014-03-20 09:00:24 +0900

    6.18(2)-before

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,34 +1,11 @@
 class Person
-  attr_reader :mother, :children, :name
+  attr_accessor :name, :age
 
-  def initialize(name, date_of_birth, date_of_death=nil, mother=nil)
-    @name, @mother = name, mother
-    @date_of_birth, @date_of_death = date_of_birth, date_of_death
-    @children = []
-    @mother.add_child(self) if @mother
+  def method_missing(sym, *args, &block)
+    empty?(sym.to_s.sub(/^empty_/, "").chomp("?"))
   end
 
-  def add_child(child)
-    @children << child
-  end
-
-  def number_of_living_descendants
-    count_descendants_matching { |descendant| descendant.alive? }
-  end
-
-  def number_of_descendants_named(name)
-    count_descendants_matching { |descendant| descendant.name == name }
-  end
-
-  protected
-  def count_descendants_matching(&block)
-    children.inject(0) do |count, child|
-      count += 1 if yield child
-      count + child.count_descendants_matching(&block)
-    end
-  end
-
-  def alive?
-    @date_of_death.nil?
+  def empty?(sym)
+    self.send(sym).nil?
   end
 end
