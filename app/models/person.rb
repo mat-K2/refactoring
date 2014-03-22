ac86311069133e817ed1511627bc147150b841dd
keiyan9 <nanbakouichi@nanba-no-MacBook-Air.local>
2014-03-22 15:17:36 +0900

    refactor move eval from runtime to parse time

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,9 +1,9 @@
 class Person
   def self.attr_with_default(options)
     options.each_pair do |attribute, default_value|
-      define_method attribute do
-        eval "@#{attribute} ||= #{default_value}"
-      end
+      eval "def #{attribute}
+        @#{attribute} ||= #{default_value}
+      end"
     end
   end
 
