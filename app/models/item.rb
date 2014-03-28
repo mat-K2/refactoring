590f5d617f87bc30163bb9ac6a168adb357e6359
keiyan9 <nanbakouichi@nanba-no-MacBook-Air.local>
2014-03-28 09:45:42 +0900

    refactor self encapsulate field

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item
+  attr_accessor :base_price, :tax_rate
 
   def initialize(base_price, tax_rate)
     @base_price = base_price
@@ -6,10 +7,10 @@ class Item
   end
 
   def raise_base_price_by(percent)
-    @base_price = @base_price * (1 + percent / 100.0)
+    self.base_price = base_price * (1 + percent / 100.0)
   end
 
   def total
-    @base_price * (1 + @tax_rate)
+    base_price * (1 + tax_rate)
   end
 end
