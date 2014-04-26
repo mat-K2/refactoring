3b054055d9130c150b9daf22e91c4d7e50d5c767
keiyan9 <nanbakouichi@nanba-no-MacBook-Air.local>
2014-04-26 17:06:22 +0900

    8.7-before

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,14 +1,4 @@
 # -*- coding: utf-8 -*-
 class Order
-  def initialize(customer_name)
-    @customer = Customer.with_name(customer_name)
-  end
-
-  def customer_name
-    @customer.name
-  end
-
-  def customer=(customer_name)
-    @customer = Customer.new(customer_name)
-  end
+  attr_accessor :customer
 end
--- spec/model/order_spec.rb
+++ spec/model/order_spec.rb
@@ -1,4 +1,13 @@
 require 'spec_helper'
 
 describe Order do
+  let(:order) { Order.new }
+
+  describe 'attr_accessor' do
+    let(:customer_1) { Customer.new("john") }
+    before do
+      order.customer = customer_1
+    end
+    it { order.customer.should == customer_1 }
+  end
 end
