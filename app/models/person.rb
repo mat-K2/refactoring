ca7f7b4925a749c30a9954b2ef19d26417f944bd
keiyan9 <nanbakouichi@nanba-no-MacBook-Air.local>
2014-04-30 08:47:53 +0900

    refactor encapsulate collection

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,22 @@
 class Person
-  attr_accessor :courses
+  def initialize
+    @courses = []
+  end
+
+  def add_course(course)
+    @courses << course
+  end
+
+  def remove_course(course)
+    @courses.delete(course)
+  end
+
+  def initialize_courses(courses)
+    raise "Courses should be empty" unless @courses.empty?
+    @courses += courses
+  end
+
+  def courses
+    @courses.dup
+  end
 end
--- spec/model/person_spec.rb
+++ spec/model/person_spec.rb
@@ -3,22 +3,30 @@ require 'spec_helper'
 describe Person do
   let(:kent) { Person.new }
 
-  describe 'courses=' do
+  describe 'initialize_courses(courses)' do
     before do
-      courses = []
-      courses << Course.new("Smalltalk Programming", false)
-      courses << Course.new("Appreciating Single Malts", true)
-      kent.courses = courses
+      @courses = []
+      @courses << Course.new("Smalltalk Programming", false)
+      @courses << Course.new("Appreciating Single Malts", true)
     end
-    it { kent.courses.count.should == 2 }
+    context 'courses is empty' do
+      before do
+        kent.initialize_courses(@courses)
+      end
+      it { kent.courses.count.should == 2 }
+    end
+    context 'courses is present' do
+      before do
+        kent.add_course(Course.new("Lisp Programming", false))
+      end
+      specify { expect { kent.initialize_courses(@courses) }.to raise_error(RuntimeError, "Courses should be empty") }
+    end
   end
 
   describe 'add course' do
     before do
-      kent.courses = []
-      refactoring = Course.new("Refactoring", true)
-      kent.courses << refactoring
-      kent.courses << Course.new("Brutal Sarcasm", false)
+      kent.add_course(Course.new("Refactoring", true))
+      kent.add_course(Course.new("Brutal Sarcasm", false))
     end
     it { kent.courses.size.should == 2 }
   end
@@ -26,8 +34,8 @@ describe Person do
   describe 'remove course' do
     before do
       refactoring = Course.new("Refactoring", true)
-      kent.courses = [refactoring, Course.new("Brutal Sarcasm", false)]
-      kent.courses.delete(refactoring)
+      kent.initialize_courses([refactoring, Course.new("Brutal Sarcasm", false)])
+      kent.remove_course(refactoring)
     end
     it { kent.courses.size.should == 1 }
   end
