af3989cd65c3e98a3b40f32893da407ce53b0261
keiyan9 <nanbakouichi@nanba-no-MacBook-Air.local>
2014-05-07 08:12:53 +0900

    refactor replace type code with polymorphism

---
+++ app/models/rigid_mountain_bike.rb
@@ -0,0 +1,11 @@
+class RigidMountainBike
+  include MountainBike
+
+  def price
+    (1 + @commission) * @base_price
+  end
+
+  def off_road_ability
+    @tire_width * TIRE_WIDTH_FACTOR
+  end
+end
--- spec/model/mountain_bike_spec.rb
+++ spec/model/mountain_bike_spec.rb
@@ -2,14 +2,12 @@ require 'spec_helper'
 
 describe MountainBike do
   let(:instance_params_of_rigid) { {
-      :type_code => :rigid,
       :tire_width => 2.5,
       :commission => 0.5,
       :base_price => 10000
     }
   }
   let(:instance_params_of_front_suspension) { {
-      :type_code => :front_suspension,
       :tire_width => 3.0,
       :commission => 1.0,
       :base_price => 15000,
@@ -18,7 +16,6 @@ describe MountainBike do
     }
   }
   let(:instance_params_of_full_suspension) { {
-      :type_code => :full_suspension,
       :tire_width => 4.0,
       :commission => 1.5,
       :base_price => 20000,
@@ -33,15 +30,15 @@ describe MountainBike do
     subject { mountain_bike.off_road_ability }
 
     context 'rigid' do
-      let(:mountain_bike) { MountainBike.new(instance_params_of_rigid) }
+      let(:mountain_bike) { RigidMountainBike.new(instance_params_of_rigid) }
       it { should == 2.5 }
     end
     context 'front_suspension' do
-      let(:mountain_bike) { MountainBike.new(instance_params_of_front_suspension) }
+      let(:mountain_bike) { FrontSuspensionMountainBike.new(instance_params_of_front_suspension) }
       it { should == 13 }
     end
     context 'full_suspension' do
-      let(:mountain_bike) { MountainBike.new(instance_params_of_full_suspension) }
+      let(:mountain_bike) { FullSuspensionMountainBike.new(instance_params_of_full_suspension) }
       it { should == 38 }
     end
   end
@@ -50,15 +47,15 @@ describe MountainBike do
     subject { mountain_bike.price }
 
     context 'rigid' do
-      let(:mountain_bike) { MountainBike.new(instance_params_of_rigid) }
+      let(:mountain_bike) { RigidMountainBike.new(instance_params_of_rigid) }
       it { should == 15000 }
     end
     context 'front_suspension' do
-      let(:mountain_bike) { MountainBike.new(instance_params_of_front_suspension) }
+      let(:mountain_bike) { FrontSuspensionMountainBike.new(instance_params_of_front_suspension) }
       it { should == 35000 }
     end
     context 'full_suspension' do
-      let(:mountain_bike) { MountainBike.new(instance_params_of_full_suspension) }
+      let(:mountain_bike) { FullSuspensionMountainBike.new(instance_params_of_full_suspension) }
       it { should == 63000 }
     end
   end
