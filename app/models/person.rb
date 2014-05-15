cf3b8714d3dfb1eef4bb822cf0b246f4c911d962
keiyan9 <nanbakouichi@nanba-no-MacBook-Air.local>
2014-05-16 08:17:59 +0900

    refactor replace subclass with fields

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,24 @@
 class Person
+
+  def initialize(female, code)
+    @female = female
+    @code = code
+  end
+
+  def self.create_female
+    Person.new(true, 'F')
+  end
+
+  def self.create_male
+    Person.new(false, 'M')
+  end
+
+  def female?
+    @female
+  end
+
+  def code
+    @code
+  end
+
 end
--- spec/model/person_spec.rb
+++ spec/model/person_spec.rb
@@ -1,4 +1,15 @@
 require 'spec_helper'
 
 describe Person do
+  let(:bree) { Person.create_female }
+
+  describe 'female?' do
+    subject { bree.female? }
+    it { should be_true }
+  end
+
+  describe 'code' do
+    subject { bree.code }
+    it { should == 'F' }
+  end
 end
