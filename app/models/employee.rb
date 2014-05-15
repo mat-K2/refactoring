892318837c0d16793cc6234ab07b4cd024f5e038
keiyan9 <nanbakouichi@nanba-no-MacBook-Air.local>
2014-05-16 08:33:00 +0900

    8.16-before

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,34 +1,8 @@
 class Employee
-  attr_reader :manager_role, :office, :salary
+  attr_reader :emails, :voice_mails
 
-  def initialize(manager_role, office, salary)
-    @manager_role = manager_role
-    @office = office
-    @salary = salary
-  end
-
-  def manager?
-    self.manager_role
-  end
-
-  def self.managers(employees)
-    employees.select { |e| e.manager? }
-  end
-
-  def self.not_managers(employees)
-    employees.reject { |e| e.manager? }
-  end
-
-  def self.offices_of_all(employees)
-    employees.map { |e| e.office }
-  end
-
-  def self.offices_of_managers(employees)
-    employees.select { |e| e.manager? }.
-              map { |e| e.office }
-  end
-
-  def self.total_salary_of_all(employees)
-    employees.inject(0) { |sum, e| sum + e.salary }
+  def initialize
+    @emails = []
+    @voice_mails = []
   end
 end
--- spec/model/employee_spec.rb
+++ spec/model/employee_spec.rb
@@ -1,34 +1,15 @@
 require 'spec_helper'
 
 describe Employee do
-  let(:employee_1){ Employee.new(false, 'CompanyA', 10) }
-  let(:employee_2){ Employee.new(false, 'CompanyB', 20) }
-  let(:manager_1){ Employee.new(true, 'CompanyC', 30) }
-  let(:manager_2){ Employee.new(true, 'CompanyD', 40) }
-  let(:employees){ [employee_1, employee_2, manager_1, manager_2] }
+  let(:employee) { Employee.new }
 
-  describe 'self.managers(employees)' do
-    subject{ Employee.managers(employees) }
-    it{ should == [manager_1, manager_2] }
+  describe 'emails' do
+    subject { employee.emails }
+    it { should == [] }
   end
 
-  describe 'self.not_managers(employees)' do
-    subject{ Employee.not_managers(employees) }
-    it{ should == [employee_1, employee_2] }
-  end
-
-  describe 'self.offices_of_all(employees)' do
-    subject{ Employee.offices_of_all(employees) }
-    it{ should == ['CompanyA', 'CompanyB', 'CompanyC', 'CompanyD'] }
-  end
-
-  describe 'self.offices_of_managers(employees)' do
-    subject{ Employee.offices_of_managers(employees) }
-    it{ should == ['CompanyC', 'CompanyD'] }
-  end
-
-  describe 'self.total_salary_of_all(employees)' do
-    subject{ Employee.total_salary_of_all(employees) }
-    it{ should == 100 }
+  describe 'voice_mails' do
+    subject { employee.voice_mails }
+    it { should == [] }
   end
 end
